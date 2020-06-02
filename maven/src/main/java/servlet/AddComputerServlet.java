d4e17f4b8d5c33e7004f04ee26328a3e17349df3
Andro <adadic@excilys.com>
2020-06-02 18:01:10 +0200

    add work (redirect but not added comp)

--- maven/src/main/java/servlet/AddComputerServlet.java
+++ maven/src/main/java/servlet/AddComputerServlet.java
@@ -3,6 +3,7 @@ package servlet;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import mapper.DateMapper;
 import model.Company;
+import model.Computer;
+import model.Computer.ComputerBuilder;
 import persistence.DAOCompany;
 
 
@@ -36,5 +40,15 @@ public class AddComputerServlet extends HttpServlet{
 			}
 			request.setAttribute("companies", companies);
 	    	this.getServletContext().getRequestDispatcher("/WEB-INF/addComputer.jsp").forward(request,response);
+	    }
+	    
+	    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    	DateMapper dateMapper = new DateMapper();
+			Computer computer = new Computer.ComputerBuilder(request.getParameter("computerName")).build();
+			response.sendRedirect("dashboard");
+//
+//			.introduced(dateMapper.getDate(request.getParameter("computerName")))
+//			.discontinued(dateMapper.getDate(request.getParameter("computerName")))
+//			.company(new Company.CompanyBuilder(request.getParameter("computerName"), request.getParameter("computerName")).build())
 	    }
 }
