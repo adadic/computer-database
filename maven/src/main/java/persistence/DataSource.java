740626fbb1130d776ac74ea979007553525929f2
Andro <andro.dadic@insa-cvl.fr>
2020-06-05 11:44:55 +0200

    dbUnit add HikariCP

---
+++ maven/src/main/java/persistence/DataSource.java
@@ -0,0 +1,46 @@
+package persistence;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class DataSource {
+	private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
+ 
+    static {
+        config.setJdbcUrl( "jdbc:mysql://localhost:3306/computer-database-db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC" );
+        config.setUsername( "admincdb" );
+        config.setPassword( "qwerty1234" );
+        config.addDataSourceProperty( "cachePrepStmts" , "true" );
+        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
+        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
+        ds = new HikariDataSource( config );
+    }
+ 
+    private DataSource() {
+    	
+    }
+ 
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+    
+//    private static HikariConfig config = new HikariConfig("src/main/java/res/hikari.properties");
+//    private static HikariDataSource ds;
+// 
+//    static {
+//        ds = new HikariDataSource(config);
+//    }
+// 
+//    private DataSource() {
+//    	
+//    }
+// 
+//    public static Connection getConnection() throws SQLException {
+//        return ds.getConnection();
+//    }
+//    
+}
--- maven/src/main/java/persistence/MysqlConnect.java
+++ maven/src/main/java/persistence/MysqlConnect.java
@@ -1,6 +1,6 @@
 package persistence;
 import java.sql.Connection;
-import java.sql.DriverManager;
+//import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,16 +10,16 @@ public final class MysqlConnect implements AutoCloseable{
 	private Connection conn;
 	private static MysqlConnect db;
 	
-	private String url = "jdbc:mysql://localhost:3306/computer-database-db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	//private String url = "jdbc:mysql://localhost:3306/computer-database-db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private String driver = "com.mysql.cj.jdbc.Driver";
-	private String userName = "admincdb";
-	private String password = "qwerty1234";
+	//private String userName = "admincdb";
+	//private String password = "qwerty1234";
 	
 	private MysqlConnect() {
 		
 		try {
 			Class.forName(driver).newInstance();
-			this.conn = DriverManager.getConnection(url, userName, password);
+			this.conn = DataSource.getConnection();
 		}
 		catch(Exception sqle){
 			sqle.printStackTrace();
