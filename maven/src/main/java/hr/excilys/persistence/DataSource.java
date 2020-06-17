0b420daa58cfc86463748001520b15244da4eb8b
Andro <andro.dadic@insa-cvl.fr>
2020-06-17 18:02:05 +0200

    HikariCP config with Spring using Mr Beans

--- maven/src/main/java/hr/excilys/persistence/DataSource.java
+++ maven/src/main/java/hr/excilys/persistence/DataSource.java
@@ -3,16 +3,19 @@ package hr.excilys.persistence;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+
+import hr.excilys.main.SpringConfiguration;
 
 @Component
 public class DataSource {
 
-	private static HikariConfig config = new HikariConfig("/hikari.properties");
-	private static HikariDataSource ds = new HikariDataSource(config);
+	static ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+	private static HikariDataSource ds = applicationContext.getBean(HikariDataSource.class);
 
 	private DataSource() {
 	}
