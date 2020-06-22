e2e8e324d1b71423bada298901f16b47766f1e0d
Andro <andro.dadic@insa-cvl.fr>
2020-06-22 10:53:57 +0200

    scan base packages specified

--- maven/src/main/java/hr/excilys/main/SpringConfiguration.java
+++ maven/src/main/java/hr/excilys/main/SpringConfiguration.java
@@ -12,19 +12,20 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-@ComponentScan("hr.excilys")
+@ComponentScan(basePackages = { "hr.excilys.dto", "hr.excilys.mapper", "hr.excilys.model", "hr.excilys.persistence",
+		"hr.excilys.service", "hr.excilys.servlet", "hr.excilys.validator" })
 @ImportResource("classpath:/applicationContext.xml")
 public class SpringConfiguration {
 
 	@Bean(destroyMethod = "close")
-	public DataSource dataSource(){
-		
-	    HikariConfig hikariConfig = new HikariConfig("/hikari.properties");
-	    HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+	public DataSource dataSource() {
 
-	    return dataSource;
+		HikariConfig hikariConfig = new HikariConfig("/hikari.properties");
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+		return dataSource;
 	}
-	
+
 	@Bean
 	NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource datasource) {
 		return new NamedParameterJdbcTemplate(datasource);
