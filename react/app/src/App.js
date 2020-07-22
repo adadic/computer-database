4c0b5ba6138d9e9e1ece9fd9cb306ff423710fee
Andro <andro.dadic@insa-cvl.fr>
2020-07-22 17:13:03 +0200

    First Header

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './App.css';
+import 'fontsource-roboto';
+import './App.scss';
+import Header from "./Component/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
     </div>
   );
 }
