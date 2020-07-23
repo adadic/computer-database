02d1407a09a8a3f674f194b8f8e5128fe018351b
Andro <andro.dadic@insa-cvl.fr>
2020-07-23 12:15:34 +0200

    Tester REST

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import 'fontsource-roboto';
 import './App.scss';
 import Header from "./Component/Header/Header";
+import TableList, {baseURL} from "./Component/TableList/TableList";
+import {MOCK} from "./Mock";
+import useAxios from "axios-hooks";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+
+
+    const {data} = useAxios(baseURL + "/computers");
+    const [computers, setComputers] = useState(data);
+    useEffect(() => setComputers(data),data);
+
+    return (
+        <div className="App">
+            <Header/>
+
+            <div>
+                Console :
+                <br/>
+                {computers === undefined ? "True" : "False"}
+                <br/>
+                {computers}
+            </div>
+        </div>
+    );
 }
 
 export default App;
