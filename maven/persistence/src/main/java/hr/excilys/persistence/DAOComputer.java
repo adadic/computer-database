bd51bb0d515a5d40cd36efafecd106727e816d8b
shoungyu2 <ysabourin78@gmail.com>
2020-07-24 11:01:08 +0200

    suppression warnings

--- maven/persistence/src/main/java/hr/excilys/persistence/DAOComputer.java
+++ maven/persistence/src/main/java/hr/excilys/persistence/DAOComputer.java
@@ -22,12 +22,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import hr.excilys.model.Computer;
 
 @Repository
 @Transactional
