faeca06679c2e91ad6d50b66241a91def784cc00
Julien <jrousseau@excilys.com>
2020-07-24 11:57:35 +0200

    conflict

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -3,6 +3,14 @@ import 'fontsource-roboto';
 import './App.scss';
 import Header from "./Component/Header";
 
+import ComputerListDelete from "./Component/ComputerListDelete";
+import useAxios from "axios-hooks";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import ErrorPage from "./Component/ErrorPage";
+
+export const baseURL = 'http://localhost:8080/webapp/api';
+
+
 function App() {
 
     return (
