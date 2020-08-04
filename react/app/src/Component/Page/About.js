f6257eee420c7e89422ff9f7a288d7be97582604
Saad BENDAOUD <saad.benda@gmail.com>
2020-08-04 17:30:41 +0200

    final version

--- react/app/src/Component/Page/About.js
+++ react/app/src/Component/Page/About.js
@@ -5,6 +5,11 @@ import Grid from '@material-ui/core/Grid';
 import ProfileUi from 'react-profile-card';
 import saad from './resources/saad.jpeg';
 import andro from './resources/andro.jpg';
+import yves from './resources/yves.jpg';
+import julien from './resources/julien.jpg';
+import imageOne from "./resources/imageOne.png";
+import imageTwo from "./resources/imageTwo.png";
+import BackgroundSlider from "react-background-slider";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,51 +22,67 @@ const useStyles = makeStyles((theme) => ({
         color: theme.palette.text.secondary,
     },
 }));
+const styles={
+    text:{
+        color:'white',
+        fontSize:'300%',
+        backgroundColor:'rgba(0,0,0,0.2)',
 
 
-function About(){
+    },
+
+
+}
+const image1 = 'https://i.pinimg.com/originals/7a/bd/12/7abd1279564c43a005ecddedd9d371fe.jpg'
+const image2 ='https://wallpaperaccess.com/full/1180986.jpg'
+    function About(){
     const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <Grid container spacing={10}>
+        <div className={classes.root} >
+            <BackgroundSlider
+                images={[image1, image2]}
+                duration={100} transition={2} />
+             <p style={styles.text}>The Dev Team</p>
+
+            <Grid container style={{flexDirection:"row",justifyContent:'space-between'}}>
                 <Grid item>
-                    <Paper>
+
                     <ProfileUi
-                        imgUrl={saad}
+                        imgUrl={julien}
                         name='Julien'
-                        designation='Developer'
+                        designation='Developper'
                     />
-                    </Paper>
+
                 </Grid>
                 <Grid item>
-                    <Paper >
+
                         <ProfileUi style={{padding:0}}
                             imgUrl={saad}
                             name='Saad'
-                            designation='Developer'
+                            designation='Developper'
                         />
-                    </Paper>
+
 
                 </Grid>
 
                 <Grid item>
-                    <Paper>
+
                     <ProfileUi
                         imgUrl={andro}
                         name='Andro'
-                        designation='Developer'
+                        designation='Developper'
                     />
-                </Paper>
+
                 </Grid>
 
                 <Grid item>
-                    <Paper>
+
                     <ProfileUi
-                        imgUrl={saad}
+                        imgUrl={yves}
                         name='Yves'
-                        designation='Developer'
+                        designation='Developper'
                     />
-                        </Paper>
+
                 </Grid>
             </Grid>
         </div>
