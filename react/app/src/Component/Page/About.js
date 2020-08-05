c98c9b7044ac827af906c001e014880d3875ba16
Andro <andro.dadic@insa-cvl.fr>
2020-08-05 09:55:19 +0200

    about scss

--- react/app/src/Component/Page/About.js
+++ react/app/src/Component/Page/About.js
@@ -1,66 +1,57 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import ProfileUi from 'react-profile-card';
 import saad from './resources/saad.jpeg';
 import andro from './resources/andro.jpg';
 import yves from './resources/yves.jpg';
 import julien from './resources/julien.jpg';
-import imageOne from "./resources/imageOne.png";
-import imageTwo from "./resources/imageTwo.png";
 import BackgroundSlider from "react-background-slider";
-
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
+        width: '70%',
+        margin: "auto"
     },
 }));
-const styles={
-    text:{
-        color:'white',
-        fontSize:'300%',
-        backgroundColor:'rgba(0,0,0,0.2)',
 
-
+const styles = {
+    text: {
+        color: 'white',
+        fontSize: '300%',
+        backgroundColor: 'rgba(0,0,0,0.2)',
     },
-
-
 }
 const image1 = 'https://i.pinimg.com/originals/7a/bd/12/7abd1279564c43a005ecddedd9d371fe.jpg'
-const image2 ='https://wallpaperaccess.com/full/1180986.jpg'
-    function About(){
+const image2 = 'https://wallpaperaccess.com/full/1180986.jpg'
+
+function About() {
     const classes = useStyles();
     return (
-        <div className={classes.root} >
+        <div className={classes.root}>
             <BackgroundSlider
                 images={[image1, image2]}
-                duration={100} transition={2} />
-             <p style={styles.text}>The Dev Team</p>
+                duration={100} transition={2}/>
+            <p style={styles.text}>The Dev Team</p>
 
-            <Grid container style={{flexDirection:"row",justifyContent:'space-between'}}>
+            <Grid container style={{flexDirection: "row", justifyContent: 'space-between'}}>
                 <Grid item>
 
                     <ProfileUi
                         imgUrl={julien}
                         name='Julien'
-                        designation='Developper'
+                        designation='Developer'
                     />
 
                 </Grid>
                 <Grid item>
 
-                        <ProfileUi style={{padding:0}}
-                            imgUrl={saad}
-                            name='Saad'
-                            designation='Developper'
-                        />
+                    <ProfileUi style={{padding: 0}}
+                               imgUrl={saad}
+                               name='Saad'
+                               designation='Developer'
+                    />
 
 
                 </Grid>
@@ -70,7 +61,7 @@ const image2 ='https://wallpaperaccess.com/full/1180986.jpg'
                     <ProfileUi
                         imgUrl={andro}
                         name='Andro'
-                        designation='Developper'
+                        designation='Developer'
                     />
 
                 </Grid>
@@ -80,7 +71,7 @@ const image2 ='https://wallpaperaccess.com/full/1180986.jpg'
                     <ProfileUi
                         imgUrl={yves}
                         name='Yves'
-                        designation='Developper'
+                        designation='Developer'
                     />
 
                 </Grid>
